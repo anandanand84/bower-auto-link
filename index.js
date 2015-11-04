4bceaedd0702ae9c100ea58d8693dd7679b941fd
Anand Aravindan <anand.anand84@gmail.com>
2015-11-04 07:56:57 -0600

    Adding binaries for global

---
+++ index.js
@@ -0,0 +1,68 @@
+var walk    = require('walk');
+var path    = require('path');
+var execSync    = require('child_process').execSync;
+
+var LINK_MODULE_COMMAND = 'bower link -f';
+var ADD_LINK_COMMAND    = 'bower link ';
+
+//var isWin = /^win/.test(process.platform);
+//if (isWin){
+//
+//} else {
+//
+//}
+
+console.log(process.platform)
+
+
+var availableProjectsWalker  = walk.walk(process.cwd(), { followLinks: false });
+
+var availableProjectsForLink = [];
+
+console.log('Finding bower modules to link in directory '+__dirname);
+
+availableProjectsWalker.on("directories", function (root, dirStatsArray, next) {
+    var linkedProjectCount = 0;
+    dirStatsArray.forEach(function(stats){
+        var moduleDirectory = path.join(root,stats.name);
+        if(isBowerModule(moduleDirectory)){
+            linkedProjectCount++;
+            availableProjectsForLink.push(stats.name);
+            var output = execSync(LINK_MODULE_COMMAND,{'cwd':moduleDirectory});
+            process.stdout.write(output);
+        }
+    });
+
+    if(linkedProjectCount < 0){
+        console.err("No Bower modules found in the directory");
+    }else{
+        console.log(linkedProjectCount +" projects available to link");
+    }
+
+    availableProjectsForLink.forEach(function(availableProject){
+        "use strict";
+        var currentProjectPath = path.join(root, availableProject);
+        var dependents = require(path.join(currentProjectPath,'bower.json')).dependencies;
+        Object.keys(dependents).forEach(function(dependency){
+            if(availableProjectsForLink.indexOf(dependency)!=-1){
+                var output = execSync(ADD_LINK_COMMAND+dependency,{'cwd':currentProjectPath});
+                process.stdout.write(output);
+            }
+        });
+    });
+});
+
+
+var isBowerModule = function(moduleDirectory){
+    "use strict";
+    try {
+        return require(path.join(moduleDirectory,'bower.json'));
+    } catch (e) {
+        if (e.code !== 'MODULE_NOT_FOUND') {
+            // Re-throw not "Module not found" errors
+            throw e;
+        }else{
+            return false;
+        }
+    }
+};
